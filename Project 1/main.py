aa0e8f298479d5d3f25fc3f7c53e38c21dd8b6a3
Radiocephas <akinnusioluwatobiloba@gmail.com>
2025-03-22 22:25:21 +0100

    I revisited the course

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,2 +1,31 @@
 print("hello world")
 print("Hey there")
+
+# V A R I A B L E S
+
+#STRINGS
+
+first_name = "Bro"
+email = "radiocephas@gmail.com"
+
+print(f"Hello {first_name}")
+print(f"your email is {email}")
+
+#INTEGERS
+
+age = 25
+num_of_students = 30
+
+print(f"you are {age} years old and you belong to a class of {num_of_students} students")
+
+#FLOAT
+
+my_pi = 22/7
+
+print(my_pi)
+
+#BOOLEAN
+
+is_alive = True
+
+print(f"Are you alive?: {is_alive}")
