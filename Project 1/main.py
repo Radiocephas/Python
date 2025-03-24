053e3ac25e85ca42d5d5ad2ad2ae0d7487dfd6c7
Radiocephas <akinnusioluwatobiloba@gmail.com>
2025-03-24 13:43:25 +0100

    Jumped on w3

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,5 +1,28 @@
-num = -3
-name = "Ologunagba"
-print("Positive" if num  > 0 else "Negative")
-print(name.find("O"))
-print(name.lower())
+import random
+
+print (random)
+
+# x = b"awesome"
+
+# def myfunc():
+    
+#     print(f"Python is {x.capitalize()}")
+
+# myfunc()
+
+# print (x)
+
+#If you create a variable with the same name inside a function, this variable will be local, and can only be used inside the function. The global variable with the same name will remain as it was, global and with the original value.
+
+a = """This is my beloved son
+in him i am well pleased,
+Hear ye Him!"""
+
+print(a[:60])
+print(a.strip())
+print(a.replace("i", "o"))
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
