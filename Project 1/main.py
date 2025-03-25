37ff9f34b7eb4740572f39ee4f493e78ed47d19d
Radiocephas <akinnusioluwatobiloba@gmail.com>
2025-03-25 20:39:59 +0100

    Leanrt how to insert, extend and append

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,5 +1,7 @@
 thislist = ["apple", "banana", "cherry"]
 thislist.append("goatmeat")
 roommates = ["Tega", "Samseph", "Ben"]
+roommates.insert(0, "Mango")
 thislist.extend(roommates)
+
 print(thislist)
